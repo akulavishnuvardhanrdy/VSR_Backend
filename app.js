6adec03ca1416ae8e7b55fa1ddf6d5ab3ae17eca
Vishnu <akulavishnuvardhanrdy@gmail.com>
2024-12-29 11:55:15 +0530

    Hosting Set Up

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./dbConn'); 
-const { AddUser, Booking ,UserLogin } = require('./ApiFunctions');
-const authenticateToken = require('./Middlewares')
+const { AddUser, Booking, UserLogin } = require('./ApiFunctions');
+const authenticateToken = require('./Middlewares');
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+// Configure CORS to allow only the specified origin
+app.use(cors({
+  origin: 'https://vsr-booking.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(bodyParser.json());
 
 connectDB();
 
 app.post('/addUser', AddUser);
-app.post('/booking',authenticateToken, Booking);
-app.post('/login',UserLogin);
+app.post('/booking', authenticateToken, Booking);
+app.post('/login', UserLogin);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
